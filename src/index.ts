8937fe373f5571200cc84210f41a98ce11f06a79
Piero Narciso <pieronarciso988@gmail.com>
2022-10-05 19:49:54 -0500

    added: linter

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import { add } from './lib';
 
 const app = express();
 
-const PORT = 8080
+const PORT = 8080;
 
 app.get('/', (_, res) => {
-  return res.send({ message: 'hello world!'});
+  return res.send({ message: 'Hello World!' });
+});
+
+app.get('/another', (_, res) => {
+  const value = add(1, 3);
+  return res.send({ message: value });
 });
 
 app.listen(PORT, () => {
